c8e831f33f2cdba1b14909933a144a81374eafcf
acarmfwd <acarmona@fwdcostarica.com>
2024-08-14 14:29:20 -0600

    Se termino con el ejercicio3

--- src/ejercicio3.js
+++ src/ejercicio3.js
@@ -1,6 +1,30 @@
-// Inserte el código aquí
 
+//Clonar Arreglo
 
+function clonarArreglo(array) {
+    let newArray=array.map((x) => x = x);
+    console.log('Nuevo arreglo', newArray);   
+}
+clonarArreglo([2,4,5,2,5,7,8]); 
 
-// NO MODIFICAR
-export { clonarArreglo, clonarObjeto, sumar, saludar };
+//Clonar Obj
+
+let Objeto={
+    nombre:"Ale",
+    apellido:"Carmona"
+}
+function clonarObjeto(obj) {
+    let newObj= {...obj};
+    console.log("Nuevo objeto",newObj);
+}
+clonarObjeto(Objeto);
+
+//Sumar
+let numeros=[1,5,1,2,3];
+let sumar=(...lista)=>{return lista.reduce((acumulador,numero)=>acumulador+numero,0)}
+console.log(sumar(...numeros));
+
+ //Saludar
+let saludar=(Nombre)=>{ return "Hola " + (Nombre||"Desconocido")}
+console.log(saludar("John"));
+
