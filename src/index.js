e86ceab40a7869c2ec2d40c60172901c4b899ef2
acarmfwd <acarmona@fwdcostarica.com>
2024-08-16 10:17:39 -0600

    Se finalizo con el ejercicio 4

--- src/index.js
+++ src/index.js
@@ -1,2 +1,25 @@
 // Inserte el código aquí
+let contenedorT= document.getElementById("timeCont");
+let updateTime=()=>{contenedorT.innerHTML=Date()}
 
+setInterval(updateTime, 1000);
+
+async function obtenerUsuarios() {
+    return new Promise(async(resolve,reject)=>{
+        let worked= await fetch("http://localhost:3000/users");
+        let usuarios=worked.json();
+        if (worked) { 
+           return resolve(usuarios)
+        }
+        reject (new Error("No entro"))
+    })
+}
+async function tries() {
+    try {
+        let usuarios= await obtenerUsuarios();
+        console.log(usuarios);
+    } catch (error) {
+        console.log("La hemos cagado"); 
+    }
+}
+tries()
